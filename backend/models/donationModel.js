d13b522ea693aa26a0a46d1bd15dd1870a07ae9e
Allen Saji <allensaji04@gmail.com>
2024-02-16 18:26:00 +0530

    featt: add donation model

---
+++ backend/models/donationModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const donationSchema = mongoose.Schema(
+  {
+    supplier_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
+      required: [true, "Please add a supplier ID"],
+    },
+    charity_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Charity",
+    },
+    itemName: {
+      type: String,
+      required: [true, "Please add an item name"],
+    },
+    itemType: {
+      type: String,
+      required: [true, "Please add an item type(veg/non-veg)"],
+    },
+    itemQuantity: {
+      type: Number,
+      required: [true, "Please add an item quantity"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Please add a donation date"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Donation", donationSchema);
