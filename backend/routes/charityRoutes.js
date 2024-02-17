915791cf39a0970b615363a880dca26a85bcc64b
Allen Saji <allensaji04@gmail.com>
2024-02-17 09:08:11 +0530

    feat: find user by authid

--- backend/routes/charityRoutes.js
+++ backend/routes/charityRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createCharity } = require("../controllers/charityController");
+const {
+  createCharity,
+  findUserByAuthId,
+} = require("../controllers/charityController");
 
 router.post("/charity", createCharity);
 router.get("/charity/:authId", findUserByAuthId);
