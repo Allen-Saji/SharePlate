29ec10cdd80df0c42e29b602291a4d7429a672f3
Allen Saji <allensaji04@gmail.com>
2024-02-17 10:00:09 +0530

    feat: add get donations route

--- backend/routes/charityRoutes.js
+++ backend/routes/charityRoutes.js
@@ -4,7 +4,11 @@ const {
   createCharity,
   findUserByAuthId,
 } = require("../controllers/charityController");
+const {
+  getRecentDonationsForCharity,
+} = require("../controllers/donationController");
 
 router.post("/charity", createCharity);
 router.get("/charity/:authId", findUserByAuthId);
+router.get("/charity/donations/:charityId", getRecentDonationsForCharity);
 module.exports = router;
