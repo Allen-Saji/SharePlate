ee049f991875456bd27f160d959b9664f4f0230d
Allen Saji <allensaji04@gmail.com>
2024-02-17 09:50:05 +0530

    chore: add charity ids

--- backend/models/donationModel.js
+++ backend/models/donationModel.js
@@ -7,17 +7,19 @@ const donationSchema = mongoose.Schema(
       ref: "Supplier",
       required: [true, "Please add a supplier ID"],
     },
-    // charity_id: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Charity",
-    // },
+    charity_ids: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Charity",
+      },
+    ],
     itemName: {
       type: String,
       required: [true, "Please add an item name"],
     },
     itemType: {
       type: String,
-      required: [true, "Please add an item type(veg/non-veg)"],
+      required: [true, "Please add an item type (veg/non-veg)"],
     },
     itemQuantity: {
       type: Number,
